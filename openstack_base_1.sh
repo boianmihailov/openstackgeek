9a7c74a2e3842965ac7557771ade75e6ff312478
Boian Mihailov <boian.mihailov@gmail.com>
2012-08-12 16:11:11 +0300

    ipforward made persistent
    ipforward made persistent after reboot and -y to package installations

--- openstack_base_1.sh
+++ openstack_base_1.sh
@@ -2,12 +2,12 @@
 
 # Make sure only root can run our script
 if [ "$(id -u)" != "0" ]; then
-   echo "You need to be 'root' dude." 1>&2
+   echo "You need to be 'root', use sudo to run the script." 1>&2
    exit 1
 fi
 
 # install time server
-apt-get install ntp
+apt-get install -y ntp
 service ntp restart
 
 # modify timeserver configuration
@@ -18,14 +18,15 @@ sed -e "
 " -i /etc/ntp.conf
 
 # install tgt
-apt-get install tgt
+apt-get install -y tgt
 service tgt start
 
 # openiscsi-client
-apt-get install open-iscsi open-iscsi-utils
+apt-get install -y open-iscsi open-iscsi-utils
 
 # turn on forwarding
 echo 1 > /proc/sys/net/ipv4/ip_forward
+echo "net.ipv4.ip_forward = 1" > /etc/sysctl.d/01-ip-forward.conf
 
 echo "#################################################################################################
 Go edit your /etc/network/interfaces file to look something like this:
@@ -39,7 +40,7 @@ iface eth0 inet static
  gateway 10.0.1.1
  dns-nameservers 8.8.8.8
 
-auto eth1
+
 
 After you are done, do a '/etc/init.d/networking restart', then run './openstack_base_2.sh'
 
